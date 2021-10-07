0a80d79690292d4957a51ea758a59c9992ad8f19
athirajohny <athira.mannapath@pearldatadirect.com>
2021-10-07 13:09:58 +0530

    conditions

--- src/App.svelte
+++ src/App.svelte
@@ -1,40 +1,21 @@
 <script>
-	let people = [
-		{name:'Athira',color:'yellow',age:23, id:1},
-		{name:'Femi',color:'blue',age:19, id:2},
-		{name:'Felix',color:'red',age:19, id:3},
-	];
-	const handleClick = (id) => {
-		// console.log(id);
-		people = people.filter((person) => person.id!=id);
-	}
+	let num = 2;
 </script>
 
 <main>
-	<!-- manually -->
-	<!-- <div>
-		{people[1].name} <p>{people[1].color}</p>
-	</div> -->
+	{#if num > 30}
+	<p>greater than 30</p>
 
-	<!-- for each loop -->
-	{#each people as person}
-	<div>
-		<p>Hi I'm {person.name}, {person.age} years old, favourite colour is {person.color}</p>
-		<button on:click={() => handleClick(person.id)}>Delete</button>
-	</div>
+	{:else if num > 20}
+	<p>greater than 20</p>
+
 	{:else}
-	<p>There are no people to show..</p>
-	{/each}
+	<p>less than 12</p>
+	
+	{/if}
 </main>
 
 
 <style>
-	main{
-		text-align: center;
-	}
-	button{
-		background-color: blue;
-		color: white;
-	}
-	/* @media (min-width: 640px) {} */
+	
 </style>
