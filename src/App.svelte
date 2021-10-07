28519e380c8ab903bc8c0785e811315cd4d7595b
athirajohny <athira.mannapath@pearldatadirect.com>
2021-10-07 14:44:13 +0530

    components

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,16 @@
 <script>
-	let num = 2;
+	import Modal from './Modal.svelte';
+
 </script>
 
+<Modal/>
+
 <main>
-	{#if num > 30}
-	<p>greater than 30</p>
 
-	{:else if num > 20}
-	<p>greater than 20</p>
-
-	{:else}
-	<p>less than 12</p>
-	
-	{/if}
 </main>
 
-
 <style>
-	
+
 </style>
+
+
---
+++ src/Modal.svelte
@@ -0,0 +1,12 @@
+<script>
+    // if showModal is true it will display the below block else it will hide
+let showModal = true;
+</script>
+
+{#if showModal}
+<div class="backdrop">
+    <div class="modal">
+        <p>Sign up for offers!</p>
+    </div>
+</div>
+{/if}
