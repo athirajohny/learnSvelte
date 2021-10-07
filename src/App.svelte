483a6b150f07e350df277778a3c5be8d751ee8c4
athirajohny <athira.mannapath@pearldatadirect.com>
2021-10-07 11:07:02 +0530

    reactive values

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,16 @@
 <script>
-	export let name;
-	let org = 'Pearl Data Direct';
 	let color = 'blue';
+	let fname = 'Athira';
+	let lname = 'Johny';
+
+	//reactive value
+    $: fullName = `${fname} ${lname}`;
+	//reactive statments
+	$: {
+    console.log(color);
+    console.log(fullName);
+  }
 	
-
-	const handleClick = () => {
-		org = "Tablez";
-	};
-
-	const handleInput = (e) => {
-		org = e.target.value
-	};
 	const changeColor = (e) =>{
 		color = e.target.value
 	};
@@ -20,14 +20,11 @@
 
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Welcome to {org}</p>
-	<button on:click={handleClick}>Change Org</button>	
-	<!-- <input type="text" on:input={handleInput} value={org}> -->
-	<!-- data binding -->
-	<input type="text" bind:value={org}>
-	<p style="color: {color};">UI looks good in {color}</p>
-	<input type="text" bind:value={color}>
+	<p style="color: {color};"> {fullName} </p>
+	<input type="text" bind:value={color}> 
+	<input type="text" bind:value={fname}>
+	<input type="text" bind:value={lname}>
+
 
 </main>
 
@@ -35,13 +32,6 @@
 <style>
 	main{
 		text-align: center;
-	}
-	h1{
-		font-size: 20px;
-	}
-	button{
-		background-color: rgb(31, 166, 219);
-		color: white;
 	}
 	/* @media (min-width: 640px) {} */
 </style>
