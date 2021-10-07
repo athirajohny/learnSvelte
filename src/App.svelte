cc28703477e803c49fcfb203fb935307ae3bc5fe
athirajohny <athira.mannapath@pearldatadirect.com>
2021-10-07 12:57:56 +0530

    Inline event handlers

--- src/App.svelte
+++ src/App.svelte
@@ -4,21 +4,23 @@
 		{name:'Femi',color:'blue',age:19, id:2},
 		{name:'Felix',color:'red',age:19, id:3},
 	];
+	const handleClick = (id) => {
+		// console.log(id);
+		people = people.filter((person) => person.id!=id);
+	}
 </script>
-
-
-
 
 <main>
 	<!-- manually -->
-	<div>
+	<!-- <div>
 		{people[1].name} <p>{people[1].color}</p>
-	</div>
+	</div> -->
 
 	<!-- for each loop -->
 	{#each people as person}
 	<div>
 		<p>Hi I'm {person.name}, {person.age} years old, favourite colour is {person.color}</p>
+		<button on:click={() => handleClick(person.id)}>Delete</button>
 	</div>
 	{:else}
 	<p>There are no people to show..</p>
@@ -29,6 +31,10 @@
 <style>
 	main{
 		text-align: center;
+	}
+	button{
+		background-color: blue;
+		color: white;
 	}
 	/* @media (min-width: 640px) {} */
 </style>
